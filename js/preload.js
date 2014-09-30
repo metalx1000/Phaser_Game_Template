51476a4b0286b28361ccf0d01277ff5d3ab42358
Kris Occhipinti <metal_bullets@filmsbykris.com>
2014-09-30 15:29:09 -0400

    Working on load text

--- js/preload.js
+++ js/preload.js
@@ -2,6 +2,13 @@ var preload = function(game){}
 
 preload.prototype = {
 	preload: function(){ 
+            load_text = this.game.add.text(this.game.world.centerX, this.game.world.centerY + 256, 'Loading', { fill: '#ffffff' });
+            load_text.anchor.setTo(.5,.5);
+
+            this.game.load.onLoadStart.add(this.loadStart, this);
+            this.game.load.onFileComplete.add(this.fileComplete, this);
+            this.game.load.onLoadComplete.add(this.loadComplete, this);
+            
             var load_icon = this.game.add.sprite(this.game.world.width * 0.5, this.game.world.height * 0.5, 'load_ani');
             load_icon.scale.setTo(2,2);
             load_icon.anchor.setTo(.5,.5);
@@ -27,6 +34,22 @@ preload.prototype = {
                 click = this.game.add.audio("click");
                 music = this.game.add.audio("music");
                 music.play('',0,1,true);
+	},
+
+        loadComplete: function(){
 		this.game.state.start("GameTitle");
-	}
+        },
+
+        loadStart: function(){
+            //console.log("Loading...");
+            load_text.setText("Loading ...");
+
+        },
+
+        //  This callback is sent the following parameters:
+        fileComplete: function(progress, cacheKey, success, totalLoaded, totalFiles) {
+            //console.log("File Complete: " + progress + "% - " + totalLoaded + " out of " + totalFiles);
+            load_text.setText("File Complete: " + progress + "% - " + totalLoaded + " out of " + totalFiles);
+        }
+
 }
