f2c84983caefc32fce8d27897dbf2182383567c5
Kris Occhipinti <metal_bullets@filmsbykris.com>
2014-10-01 14:45:11 -0400

    Added front walk animation to theGame

--- js/thegame.js
+++ js/thegame.js
@@ -3,8 +3,17 @@ var theGame = function(game){
 
 theGame.prototype = {
   	create: function(){
-            var click_me = this.game.add.button(this.game.world.width * 0.5,this.game.world.height * 0.5,"click_me",this.clickMe,this);
+            var click_me = this.game.add.button(this.game.world.width * 0.5,this.game.world.height * 0.2,"click_me",this.clickMe,this);
             click_me.anchor.setTo(0.5,0.5);
+
+            //player animation
+            player = this.game.add.sprite(this.game.world.width * 0.5,this.game.world.height * 0.5, "playerf");
+            player.anchor.setTo(.5,.5);
+            player.scale.setTo(3,3);
+            player.animations.add('walk');
+            player.animations.play('walk', 10, true);
+            player.inputEnabled = true;
+            player.events.onInputDown.add(this.clickMe,this);
              
 	},
         clickMe: function(){
