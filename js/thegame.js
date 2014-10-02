20e30884a265a3779bd9320d3d48f8c64613eb5a
metalx1000 <metal_bullets@filmsbykris.com>
2014-10-02 17:56:24 +0000

    added tween to theGame.js and added front view for blue player

--- js/thegame.js
+++ js/thegame.js
@@ -3,21 +3,37 @@ var theGame = function(game){
 
 theGame.prototype = {
   	create: function(){
-            var click_me = this.game.add.button(this.game.world.width * 0.5,this.game.world.height * 0.2,"click_me",this.clickMe,this);
-            click_me.anchor.setTo(0.5,0.5);
-
+            var click_me = this.game.add.button(this.game.world.width * 0.5,this.game.world.height * 0.2,"click_me",this.endGame,this);
+	    click_me.anchor.setTo(0.5,0.5);
+	    this.create_player("playerf");
+	},
+        clickMe: function(player){
+            click.play();
+            tween = this.game.add.tween(player).to({ x: this.game.world.width + 500 }, 1000);
+            tween.start();           
+            //console.log(player); 
+            if(player.key == "playerf"){
+                this.create_player("playerf2");
+            }else{
+                this.create_player("playerf");
+            }
+        },
+        endGame: function(){
+            click.play();
+            this.game.state.start("GameOver");
+        },
+	create_player: function(pl){
             //player animation
-            player = this.game.add.sprite(this.game.world.width * 0.5,this.game.world.height * 0.5, "playerf");
+            //player = this.game.add.sprite(this.game.world.width * 0.5,this.game.world.height * 0.5, "playerf");
+            player = this.game.add.sprite(-256,this.game.world.height * 0.5, pl);
             player.anchor.setTo(.5,.5);
             player.scale.setTo(3,3);
             player.animations.add('walk');
-            player.animations.play('walk', 10, true);
+	    player.animations.play('walk', 10, true);
             player.inputEnabled = true;
-            player.events.onInputDown.add(this.clickMe,this);
-             
-	},
-        clickMe: function(){
-            click.play();
-            this.game.state.start("GameOver");
-        }
+	    player.events.onInputDown.add(this.clickMe,this);
+            tween = this.game.add.tween(player).to({ x: this.game.world.width * 0.5 }, 1000);
+            tween.start();
+
+	}
 }
