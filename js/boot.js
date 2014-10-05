bcc2ee6b095c53cc84293738f1e629602df42cc2
Kris Occhipinti <metal_bullets@filmsbykris.com>
2014-10-04 20:02:38 -0400

    Adjusted screen size options

--- js/boot.js
+++ js/boot.js
@@ -9,7 +9,7 @@ boot.prototype = {
   	create: function(){
 		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		this.scale.pageAlignHorizontally = true;
-		this.scale.setScreenSize();
+                this.scale.setScreenSize(true);
 		this.game.state.start("Preload");
 	}
 }
