918216f6d9038ec339a5108a5ecec2ba8b076910
Kris Occhipinti <metal_bullets@filmsbykris.com>
2014-10-13 16:50:18 -0400

    Verions updater removed itself from manifest file

--- update.sh
+++ update.sh
@@ -5,6 +5,10 @@ echo "Updating MANIFEST file for offline mode..."
 echo "CACHE MANIFEST" > site.manifest &&
 echo "" >> site.manifest &&
 echo "# version $(date +%s)" >> site.manifest && 
-find . -type f -name "*" ! -path "./.git/*" ! -path "./site.manifest" ! -path "./misc/*" >> site.manifest
+find . -type f -name "*" \
+    ! -path "./.git/*" \
+    ! -path "./site.manifest"\
+    ! -path "$0" \
+    ! -path "./misc/*" >> site.manifest
 
 echo "MANIFEST has been updated."
